d76321303a16c13394af3451b5076c18dfee67da
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2024-11-13 13:58:28 +0100

    Update Ejercicio14.java

--- Boletin/Boletin 5/Ejercicio14.java
+++ Boletin/Boletin 5/Ejercicio14.java
@@ -1,5 +1,9 @@
 package com;
-
+/**
+Definir una función que tome como parámetros dos tablas, la primera con los 6 números
+de una apuesta de la primitiva, y la segunda con los 6 números de la combinación
+ganadora. La función devolverá el número de aciertos.
+*/
 public class Ejercicio14 {
 	
 	public static int[] numeros_ganadores = {1,5,6,8,9,23};
@@ -8,16 +12,36 @@ public class Ejercicio14 {
 	
 	
 	public static void main(String [] args) {
+		int total = total_aciertos(numeros_ganadores, numeros_jugados);
+	//	int total = total_aciertos(numeros_ganadores, numeros_ganadores);
+		
+		if(total == numeros_ganadores.length) {
+				System.out.println("¡Has ganado!");
+		}else if(total > 0) {
+				System.out.println("En total has acertado: "+ total);
+		}else {
+			System.out.println("¡Qué mala suerte pisha!");
+		}
+		
+	
+	}
+	/**
+	 * Función que nos permite contar el total de aciertos del boletos
+	 * @param boleto_ganador
+	 * @param apuesta
+	 * @return
+	 */
+	public static int total_aciertos(int[] boleto_ganador, int[] apuesta) {
 		int contador = 0;
-		for(int i=0; i<numeros_ganadores.length; i++) {
-			for(int j=0; j<numeros_jugados.length; j++) {
-				if(numeros_ganadores[i] == numeros_jugados[j]) {
+		for(int i=0; i<boleto_ganador.length; i++) {
+			for(int j=0; j<apuesta.length; j++) {
+				if(boleto_ganador[i] == apuesta[j]) {
 					contador++;
 				}
 			}
 		}	
-		
-		System.out.println("El total de acertados es: " + contador);
+	return contador;	
+
 	}
 	
 
